e009cff25d1fbe41b4de3427c7ec0a4ac2d3d872
kaapomoi <kaapomo@gmail.com>
2024-04-20 14:16:04 +0300

    Add score reveal animation

--- lib/main.dart
+++ lib/main.dart
@@ -78,9 +78,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   HapticFeedback.vibrate();
                 },
                 child: scoreRevealed
-                    ? Text(
-                        '$sum',
-                        style: Theme.of(context).textTheme.displayLarge,
+                    ? DigitScrollAnimation(
+                        value: sum, // Your three-digit variable here
+                        duration: const Duration(seconds: 5),
                       )
                     : Text(
                         'reveal score',
@@ -91,6 +91,61 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DigitScrollAnimation extends StatefulWidget {
+  final int value;
+  final Duration duration;
+
+  const DigitScrollAnimation(
+      {super.key, required this.value, required this.duration});
+
+  @override
+  State<DigitScrollAnimation> createState() => _DigitScrollAnimationState();
+}
+
+class _DigitScrollAnimationState extends State<DigitScrollAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+
+    _animation = IntTween(
+      begin: 0,
+      end: widget.value,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutQuint));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        final fontSize = 24.0 + (_animation.value / 3.0);
+
+        return Text(
+          _animation.value.toString(),
+          style: TextStyle(fontSize: fontSize),
+        );
+      },
     );
   }
 }
